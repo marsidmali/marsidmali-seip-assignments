c3245fd99e24be5c8a6762a0e417f7647369fbc9
marsidmali <93339789+marsidmali@users.noreply.github.com>
2023-05-07 17:18:20 +0300

    correct invalid type of histogram                       Fixes: #3

--- gradeshistogram/src/main/java/gr/aueb/dmst/marsid/HistogramGenerator.java
+++ gradeshistogram/src/main/java/gr/aueb/dmst/marsid/HistogramGenerator.java
@@ -1,12 +1,12 @@
 package gr.aueb.dmst.marsid;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
 import org.jfree.chart.JFreeChart;
-import org.jfree.data.statistics.HistogramDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 /**
  * The HistogramGenerator class reads a file with grades and generates a histogram showing the frequency
@@ -14,74 +14,73 @@ import org.jfree.data.statistics.HistogramDataset;
  * The class uses the JFreeChart library to generate the histogram.
  *
  * @author Marsid Mali
- * @version 1.0
+ * @version 3.0
  * @since 2023-04-12
  */
 public class HistogramGenerator {
 	/**
-	 * Reads the grades from a file and returns them as an array of doubles.
+	 * Reads grades from a file and returns an array of grades.
 	 *
-	 * @param fileName the name of the file to read the grades from
-	 * @return an array of doubles representing the grades
-	 * @throws IOException if an I/O error occurs while reading the file
+	 * @param fileName the path to the grades file.
+	 * @return an array of grades read from the file.
+	 * @throws IOException if an I/O error occurs while reading the file.
 	 */
 
-	public double[] readGradesFile(String fileName) throws IOException {
-		ArrayList<Double> grades = new ArrayList<>();
-		BufferedReader reader = new BufferedReader(new FileReader(fileName));
-		String line;
-		while ((line = reader.readLine()) != null) {
-			grades.add(Double.parseDouble(line));
-		}
-		reader.close();
-		return grades.stream().mapToDouble(Double::doubleValue).toArray();
+	private static int[] readGradesFile(String fileName) throws IOException {
+		return Files.lines(Paths.get(fileName))
+				.mapToInt(Integer::parseInt)
+				.toArray();
 	}
 
 	/**
 	 * Creates a histogram dataset from an array of grades.
 	 *
-	 * @param gradesArray an array of doubles representing the grades
-	 * @return a histogram dataset containing the grades data
+	 * @param grades the array of grades.
+	 * @return the created XYSeriesCollection representing the histogram dataset.
 	 */
-	public HistogramDataset createHistogramDataset(double[] gradesArray) {
-		HistogramDataset dataset = new HistogramDataset();
-		dataset.addSeries("Grades", gradesArray, 10);
-		return dataset;
+	private static XYSeriesCollection createHistogramDataset(int[] grades) {
+		int[] frequencies = new int[11];
+		for (int grade : grades) {
+			frequencies[grade]++;
+		}
+		XYSeries data = new XYSeries("Grades");
+		for (int i = 0; i < frequencies.length; i++) {
+			data.add(i, frequencies[i]);
+		}
+		return new XYSeriesCollection(data);
 	}
 
 	/**
-	 * Creates a histogram chart from a histogram dataset.
+	 * Creates a histogram chart using the given dataset.
 	 *
-	 * @param dataset the histogram dataset to create the chart from
-	 * @return a JFreeChart object representing the histogram chart
+	 * @param dataset the dataset containing the histogram data.
+	 * @return the created JFreeChart representing the histogram chart.
 	 */
-	public JFreeChart createHistogramChart(HistogramDataset dataset) {
-		return ChartFactory.createHistogram("Grades Histogram", "Grades", "Frequency", dataset);
+	private static JFreeChart createHistogramChart(XYSeriesCollection dataset) {
+		return ChartFactory.createHistogram("Grades Histogram", "Grade", "Frequency", dataset);
 	}
 
 	/**
-	 * Displays a histogram chart in a frame.
+	 * Displays the given chart in a chart frame.
 	 *
-	 * @param chart the histogram chart to display
+	 * @param chart the chart to be displayed.
 	 */
-	public void displayChart(JFreeChart chart) {
-		ChartFrame frame = new ChartFrame("Histogram", chart);
+	private static void displayChart(JFreeChart chart) {
+		ChartFrame frame = new ChartFrame("Grades Histogram", chart);
 		frame.pack();
 		frame.setVisible(true);
 	}
 
 	/**
-	 * Main method that generates a histogram chart for a set of grades read from a file.
-	 *
-	 * @param args the command-line arguments (the name of the file to read the grades from)
-	 * @throws IOException if an I/O error occurs while reading the file
+	 * The main method that reads a file containing grades, creates a histogram dataset from the grades,
+	 * creates a histogram chart from the dataset, and displays the chart.
+	 * @param args an array of command-line arguments, where the first argument should be the path to the grades file
+	 * @throws IOException if an I/O error occurs while reading the grades file
 	 */
 	public static void main(String[] args) throws IOException {
-		HistogramGenerator generator = new HistogramGenerator();
-		double[] gradesArray = generator.readGradesFile(args[0]);
-		HistogramDataset dataset = generator.createHistogramDataset(gradesArray);
-		JFreeChart chart = generator.createHistogramChart(dataset);
-		generator.displayChart(chart);
+		int[] grades = readGradesFile(args[0]);
+		XYSeriesCollection dataset = createHistogramDataset(grades);
+		JFreeChart chart = createHistogramChart(dataset);
+		displayChart(chart);
 	}
 }
-
